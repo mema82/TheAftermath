1ab8df415f684803936ba054275a4e20af203e6d
Melissa Malpica <Malpicadz@gmail.com>
2015-08-31 22:44:21 -0400

    Adding object to the scope so that it can be accessible on the templates.

--- js/ngTodoCtrl.js
+++ js/ngTodoCtrl.js
@@ -1,4 +1,16 @@
-ngTodo.controller('ngTodoCtrl', function ngTodoCtrl($scope){
+//Each route will have it's different Controlers
+//Check out Angular UI router, not the default router
 
+ngTodo.controller('ngTodoCtrl', function ngTodoCtrl($scope){ //$scope is a dependency being injected. I can also use my custom services.
+	$scope.todos = [
+		{val: "China", completed: false},
+		{val: "Japan", completed: true},
+		{val: "Korea", completed: false},
+		{val: "Amsterdam", completed: true},
+		{val: "Europe", completed: false}
+	];
 
 });
+
+/*If you want info available on the templating side, it neesd to be on the Scope object. You can add JS
+off the Scope and it will be available but just not on template access.*/
