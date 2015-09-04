5ea67d48422b91b9ef4e874d18f5c7a09f40f722
Melissa Malpica <Malpicadz@gmail.com>
2015-09-04 14:01:38 -0400

    Added pusu functionality to the add btn

--- js/ngTodoCtrl.js
+++ js/ngTodoCtrl.js
@@ -9,6 +9,17 @@ ngTodo.controller('ngTodoCtrl', function ngTodoCtrl($scope){ //$scope is a depen
 		{val: "Amsterdam", completed: true},
 		{val: "Europe", completed: false}
 	];
+
+	//Adding some functions
+
+	$scope.addNewTask = function() {
+		//Add (push) a new task to be the value of 'newTask' in the scope obj
+		$scope.todos.push({completed: false, val: $scope.newTask });
+		//After adding task in input, clear out the input
+		$scope.newTask = '';
+
+	};
+
 });
 
 /*If you want info available on the templating side, it neesd to be on the Scope object. You can add JS
