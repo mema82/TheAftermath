6734c5a88e783b7eba95f75b649c455bc5a79aa5
Melissa Malpica <Malpicadz@gmail.com>
2015-09-07 14:58:17 -0400

    Setting up Controllers to come back and work with. Not partial becasue small app

---
+++ js/Controllers.js
@@ -0,0 +1,15 @@
+Todos.TodoController =Ember.ObjectController.extend({
+	//Will define actions that can happen to the object
+	actions: {
+
+
+	}
+});
+
+//placing both controllers her b/c short program, otherwise would make partial
+
+Todos.TodosController = Ember.ArrayController.extend ({
+	actions: {
+
+	}
+});
