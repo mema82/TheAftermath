4476577f685d5ca0365696391a96eb4e18026afe
Melissa Malpica <Malpicadz@gmail.com>
2015-09-07 22:01:34 -0400

    Adding CreateNew and Remove functions to controllers

--- js/Controllers.js
+++ js/Controllers.js
@@ -1,8 +1,11 @@
 Todos.TodoController = Ember.ObjectController.extend({
 	//Will define actions that can happen to the object
 	actions: {
-
-
+		removeTodo: function () {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
+		}
 	}
 });
 
@@ -10,6 +13,14 @@ Todos.TodoController = Ember.ObjectController.extend({
 
 Todos.TodosController = Ember.ArrayController.extend ({
 	actions: {
-
+		createNewTodo: function() {
+			var newVal = this.get('newTodo'); // gets new todo Val
+			var todo = this.store.createRecord('todo' , { //Creates a new todo type
+				val: newVal , // gets new val
+				completed: false // sets to false completed
+			});
+			this.set('newtodo', ''); // set new todo to blank and save prev value to list
+			todo.save();
+		}
 	}
 });
