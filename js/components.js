c6c88a7f20d03cce9f22d5865d121ec1f025034d
Melissa Malpica <Malpicadz@gmail.com>
2015-09-07 22:00:37 -0400

    Adding remove component

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 Todos.TodoEntryComponent = Ember.Component.extend({
 	actions: {
 		click: function(){
-			this.sendAction('remove');
+			this.sendAction('remove'); //Call remove action and passes through remove on view
 		}		
 	}
 });
