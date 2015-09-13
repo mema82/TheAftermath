448500f4d789032d07bd10a9122856d2a51d0ec1
Melissa Malpica <Malpicadz@gmail.com>
2015-09-13 00:06:50 -0400

    Added uodate func for updateing the model

--- js/models.js
+++ js/models.js
@@ -2,6 +2,10 @@
 window.TodoItem = Backbone.Model.extend({
 	toggle: function() {
 		this.set('completed', !this.get('completed'));//setting to opposite of completed
+	},
+
+	updateText: function (text) { //updated model text
+		this.set('val', text);
 	}
 }); 
 
@@ -9,6 +13,15 @@ window.TodoItem = Backbone.Model.extend({
 
 window.TodoItems = Backbone.Collection.extend({ //collection of Items
 	model: TodoItem,
+
+	initialize: function() {
+		this.on('destroy', this.removeElement, this);
+	},
+
+	removeElement: function(model) {
+		this.remove(model);
+	},
+
 	filterCompleted: function() {
 		this.remove(this.filter(function(item) { //filters completed out
 			return item.get('completed'); 
