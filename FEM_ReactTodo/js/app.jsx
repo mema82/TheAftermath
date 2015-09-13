555f64534e7b7e27df8770ecc2ca5ae3cef2324f
Melissa Malpica <Malpicadz@gmail.com>
2015-09-13 12:38:39 -0400

    Added iffys

--- FEM_ReactTodo/js/app.jsx
+++ FEM_ReactTodo/js/app.jsx
@@ -1,1 +1,12 @@
 /** @jsx React.DOM */
+
+var app = app || {}; // app = new app or new object. Use exisitng if not create one and start building
+
+
+//Wrap all in instantly invoked function expression
+
+(function() {
+	'use strict';
+	
+
+})();
--- FEM_ReactTodo/js/components.jsx
+++ FEM_ReactTodo/js/components.jsx
@@ -1,1 +1,12 @@
 /** @jsx React.DOM */
+
+var app = app || {};
+
+app.components = app.components || {}; //sometimes multiple files will be adding components
+
+//IIFE - protects agains global pollution
+
+(function() {
+	'use strict';
+
+})();
