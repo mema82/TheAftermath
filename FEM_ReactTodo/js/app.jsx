1c7c5e793d833881b42a94f11b807dae72e48b00
Melissa Malpica <Malpicadz@gmail.com>
2015-09-13 13:02:56 -0400

    init app and its up and runnng

--- FEM_ReactTodo/js/app.jsx
+++ FEM_ReactTodo/js/app.jsx
@@ -7,6 +7,17 @@ var app = app || {}; // app = new app or new object. Use exisitng if not create 
 
 (function() {
 	'use strict';
-	
+
+	app.init = function() {
+		var TodoApp = app.components.TodoApp; //components is available here b.c of declared double assignment in components (line 13)
+		React.renderComponent(
+			<TodoApp />, /*Turns components into function calls (line 12), now can be used here. 
+			Says we want a TodoApp component rendered at the app Id*/
+				document.getElementById('app')
+			);
+
+	};
+
+	app.init(); // gets app up anf running
 
 })();
