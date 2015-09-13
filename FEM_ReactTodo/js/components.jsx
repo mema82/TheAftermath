fcc59f3f008009089700320b76ce7afe2a016698
Melissa Malpica <Malpicadz@gmail.com>
2015-09-13 12:50:41 -0400

    Adding root component

--- FEM_ReactTodo/js/components.jsx
+++ FEM_ReactTodo/js/components.jsx
@@ -9,4 +9,19 @@ app.components = app.components || {}; //sometimes multiple files will be adding
 (function() {
 	'use strict';
 
+//root components
+var TodoApp = app.components.TodoApp = React.createClass({
+	render: function () {
+		return (
+			<div className="outer-container">
+				<p>Cool stuff</p>
+			</div>
+			);
+		}
+
+	});
+
 })();
+
+
+
