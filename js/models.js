352624380ac8b4674586415eee0f4fbdad5c7a4d
Melissa Malpica <Malpicadz@gmail.com>
2015-09-12 22:45:55 -0400

    Adding toggle function in models

--- js/models.js
+++ js/models.js
@@ -1,5 +1,11 @@
 //Multiple Models = mult files BUT this is a super short script
-window.TodoItem = Backbone.Model.extend({}); //1 Item
+window.TodoItem = Backbone.Model.extend({
+	toggle: function() {
+		this.set('completed', !this.get('completed'));//setting to opposite of completed
+	}
+}); 
+
+//1 Item
 
 window.TodoItems = Backbone.Collection.extend({ //collection of Items
 	model: TodoItem,
