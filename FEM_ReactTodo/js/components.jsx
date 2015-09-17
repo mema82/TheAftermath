54b96c9cd6d3434dfc6ee12cedd557cdea3a3792
Melissa Malpica <Malpicadz@gmail.com>
2015-09-16 22:17:16 -0400

    Defining Components

--- FEM_ReactTodo/js/components.jsx
+++ FEM_ReactTodo/js/components.jsx
@@ -22,6 +22,29 @@ var TodoApp = app.components.TodoApp = React.createClass({ //root component
 		}
 
 	});
+	var NewTodo = app.components.NewTodo = React.createClass({
+		render: function () {
+			return(
+				<h1>NewTodo</h1>
+				);
+		}
+	});
+
+	var TodoList = app.components.TodoList = React.createClass({
+		render: function () {
+			return(
+				<h1>Todo List</h1>
+				);
+		}
+	});
+
+	var clearCompleted = app.components.clearCompleted = React.createClass({
+		render: function () {
+			return(
+				<h1>Clear Completed</h1>
+				);
+		}
+	});
 
 })();
 
