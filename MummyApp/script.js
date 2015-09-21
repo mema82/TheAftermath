582661c1916780fb95d0ea396c3286793d3ba917
Melissa Malpica <Malpicadz@gmail.com>
2015-09-21 15:37:47 -0400

    Adding a few routes

--- MummyApp/script.js
+++ MummyApp/script.js
@@ -1,5 +1,37 @@
 //create main modulevar 
-myApp = angular.module('mummyApp', []);
+myApp = angular.module('mummyApp', ['ngRoute']);
+
+//Route Configs
+mummyApp.config(function($routeProvider) {
+	$routeProvider
+
+		//Home route
+		.when('/', {
+
+			templateUrl : 'partials/home.html' ,
+			controller : 'mainCtrl'
+		})
+
+		//Chap 1 Whoa Mummy
+		.when('/WhoaMummy.html', {
+
+			templateUrl : 'partials/WhoaMummy.html' ,
+			controller : 'chap1Ctrl'
+		})
+
+		//Chap 2 Natural Mummies
+		.when('/NaturalMummies' ,{
+
+			templateUrl: 'partials/NaturalMummies.html' ,
+			controller : 'chap2Ctrl'
+		});
+
+});
+
+
+
+
+
 
 //Scope injection
 mummyApp.controller('mainCtrl' , function($scope) {
