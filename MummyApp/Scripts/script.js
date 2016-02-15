7137b279b2982985b7cec6af2d9b6fbbcff91a1a
Melissa Malpica <Malpicadz@gmail.com>
2016-02-15 17:54:21 -0500

    Content for Chap2 added

--- MummyApp/Scripts/script.js
+++ MummyApp/Scripts/script.js
@@ -1,14 +1,5 @@
-<<<<<<< Updated upstream:MummyApp/script.js
-//create main modulevar 
-<<<<<<< HEAD:MummyApp/script.js
-mummyApp = angular.module('mummyApp', ['ngRoute']);
-=======
 //create main module 
 mummyApp = angular.module('mummyApp', ['ngRoute' , 'ngAnimate']);
->>>>>>> Stashed changes:MummyApp/Scripts/script.js
-=======
-mummyApp = angular.module('mummyApp', ['ngRoute' , 'ngAnimate']);
->>>>>>> master:MummyApp/Scripts/script.js
 
 //Route Configs
 mummyApp.config(function($routeProvider , $locationProvider) {
@@ -27,60 +18,25 @@ mummyApp.config(function($routeProvider , $locationProvider) {
 
 			templateUrl : 'Views/WhoaMummy.html' ,
 			controller : 'chap1Ctrl'
-			//css: '<link rel="stylesheet" href="/Styles/chap1.css">' WHY THIS DOES NOT WORK?
+		
 		})
 
 		//Chap 2 Natural Mummies
-		.when('/NaturalMummies' , {
+		.when('/MummyMaking' , {
 
-			templateUrl: 'Views/NaturalMummies.html' ,
+			templateUrl: 'Views/MummyMaking.html' ,
 			controller : 'chap2Ctrl'
 		});
 
 });
 
 
-<<<<<<< HEAD:MummyApp/script.js
-<<<<<<< Updated upstream:MummyApp/script.js
-//Scope injection - Routes DEFINED
-=======
-//Scope injection - Routes DEFINED in Controllers Folder
-
->>>>>>> master:MummyApp/Scripts/script.js
-mummyApp.controller('mainCtrl' , function($scope) {
-
-	//messege to show which view your on
-	$scope.title = "Home";
-
-});
-
-mummyApp.controller('chap1Ctrl' , function($scope) {
-
-	//messege to show which view your on
-	$scope.title= "Whoa Mummy!";                 
-});
-
-mummyApp.controller('chap2Ctrl' , function($scope) {
-
-	//messege to show which view your on
-	$scope.title = "This is Chap 2!";
-});
-
-<<<<<<< HEAD:MummyApp/script.js
-=======
 //Scope injection - Routes DEFINED in Controllers Folder
 
 
 //HTML5 History API - Change route of the URL w/o refreshing the page
 
 
->>>>>>> Stashed changes:MummyApp/Scripts/script.js
-=======
-//HTML5 History API - Change route of the URL w/o refreshing the page
-$locationProvider.html5Mode(true);
-
-
->>>>>>> master:MummyApp/Scripts/script.js
 
 
 
